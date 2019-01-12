d2052c0b65a7ee916890f7da927ccd6382517d8c
Jenny Lian <jennylian100@gmail.com>
2019-01-12 07:40:16 -0800

    seems to start fine

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/")
+    String home(){
+        return "Hello World";
+    }
+
+}
