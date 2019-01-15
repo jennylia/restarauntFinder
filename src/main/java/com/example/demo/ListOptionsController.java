35ed75b74ec81777a5cbdea22be5e405ac7ae9b2
Jenny Lian <jennylian100@gmail.com>
2019-01-14 21:58:10 -0800

    okay this is a good break

--- src/main/java/com/example/demo/ListOptionsController.java
+++ src/main/java/com/example/demo/ListOptionsController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import com.example.demo.yelp.Business;
 import com.example.demo.yelp.YelpClient;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 // TODO: change back to @controller once ready to bind to views
 public class ListOptionsController {
@@ -25,13 +29,21 @@ public class ListOptionsController {
         model.addAttribute("location", location);
         model.addAttribute("category", category);
 
+        List<Business> businesses = new ArrayList<>();
+        Business b1 = new Business("test1");
+        Business b2 = new Business("test2");
+        businesses.add(b1);
+        businesses.add(b2);
+
+        model.addAttribute("businesses", businesses);
+
 
         // Find me the return val:
         YelpClient yelpClient = new YelpClient();
         String yelpClientData = yelpClient.getData(location, category, limit);
 
         // TODO: change back to controller once ready to bind to views
-        return yelpClientData;
+        return "restaurantList";
 
     }
 }
