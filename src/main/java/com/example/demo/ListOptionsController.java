27b596511d6cf80f7be3b47dc39bb21efda7ae88
Jenny Lian <jennylian100@gmail.com>
2019-01-14 21:41:03 -0800

    made controller more flexible

--- src/main/java/com/example/demo/ListOptionsController.java
+++ src/main/java/com/example/demo/ListOptionsController.java
@@ -13,15 +13,22 @@ import java.net.URISyntaxException;
 @RestController
 @EnableAutoConfiguration
 // TODO: change back to @controller once ready to bind to views
-public class RestaurantFinderController {
+public class ListOptionsController {
 
     @GetMapping("listOptions")
-    public String greeting(@RequestParam(name = "location", required = false, defaultValue = "Seattle") String location, Model model) throws IOException, URISyntaxException {
+    public String greeting(
+            @RequestParam(name = "location", required = false, defaultValue = "Seattle") String location,
+            @RequestParam(name = "category", required = false, defaultValue = "sushi") String category,
+            @RequestParam(name = "limit", required = false, defaultValue = "2") int limit,
+            Model model
+    ) throws IOException, URISyntaxException {
         model.addAttribute("location", location);
+        model.addAttribute("category", category);
+
 
         // Find me the return val:
         YelpClient yelpClient = new YelpClient();
-        String yelpClientData = yelpClient.getData(location);
+        String yelpClientData = yelpClient.getData(location, category, limit);
 
         // TODO: change back to controller once ready to bind to views
         return yelpClientData;
